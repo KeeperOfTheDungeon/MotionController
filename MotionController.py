621822d2c30326b4fd7d77927d560f95beeeaf90
Oktavian Gniot <Oktavian@gniot.de>
2023-08-14 11:02:53 +0200

    runs
    Signed-off-by: Oktavian Gniot <Oktavian@gniot.de>

--- MotionController.py
+++ MotionController.py
@@ -4,60 +4,22 @@ from time import sleep
 
 from RoboControl.Com.Connection.PicoConnection import PicoConnection
 from RoboControl.Robot.HardwareDevice.HardwareDevice import HardwareDevice
-
+from RoboControl.Robot.AbstractRobot.Config.DeviceConfig import DeviceConfig
 
 
 class MotionController(HardwareDevice):
-	def __init__(self):
-		super().__init__("MotionController")
+    def __init__(self):
+        super().__init__(DeviceConfig(1, "MotionController"))
+        self.connect(PicoConnection())
 
 
-
-	def run(self):
-		while True:
+    def run(self):
+        print("device - run")
+        while True:
 #			self._data_hub.remote_ping_device()
-			sleep(1)
+            sleep(1)
 
-def connect(self, connection: PicoConnection) -> None:
-		self._connection = connection
-		#super().connect(connection)
-
-"""
-	def run(self):
-		while True:
-			self._data_hub.remote_ping_device()
-			sleep(1)
-
-
-		self.add_devices()
-
-	def add_devices(self):
-		self._data_hub = DataHub(AntDeviceConfig.MAIN_DATA_HUB)
-		self._device_list.append(self._data_hub)
-		self._data_hub.set_transmitter(self._connection)
-
-		self._head_sensors = HeadSensors(AntDeviceConfig.HEAD_SENSORS)
-		self._device_list.append(self._head_sensors)
-		self._head_sensors.set_transmitter(self._connection)
-
-		self._leg_sensors = LegSensors(AntDeviceConfig.LEG_SENSORS)
-		self._device_list.append(self._leg_sensors)
-		self._leg_sensors.set_transmitter(self._connection)
-
-		self._leg_controller = LegController(AntDeviceConfig.LEG_CONTROLLER)
-		self._device_list.append(self._leg_controller)
-		self._leg_controller.set_transmitter(self._connection)
-
-	def get_head_sensors(self):
-		return self._head_sensors
-
-	def get_leg_sensors(self):
-		return self._leg_sensors
-
-	def get_leg_controller(self):
-		return self._leg_controller
-
-	def get_data_hub(self):
-		return self._data_hub
-
-"""
+    def connect(self, connection: PicoConnection) -> None:
+        self._connection = connection
+        self._connection.connect(None)
+        pass
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from MotionController import MotionController
 
 
 def main():
+    print("hallo")
+    print("hallo")
     device = MotionController()
-
+    
     """available_ports = SerialConnection.get_ports()
     port = SerialConnection.port if SerialConnection.port in available_ports else available_ports[0].name
     connection = SerialConnection().set_port(port)
@@ -13,3 +15,4 @@ def main():
     device.run()
 
 
+main()
