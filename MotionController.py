031959a2e88a7590d8a074dc90424ccb48c7adbc
koda1026 <koda1026@h-ka.de>
2023-08-19 20:45:55 +0200

    Moved ping to loop in run method.

--- MotionController.py
+++ MotionController.py
@@ -3,6 +3,8 @@ from time import sleep
 
 
 from RoboControl.Com.Connection.PicoConnection import PicoConnection
+from RoboControl.Com.Remote.RemoteData import RemoteData
+from RoboControl.Com.Remote.RemoteDataPacket import RemoteDataPacket
 from RoboControl.Robot.HardwareDevice.HardwareDevice import HardwareDevice
 from RoboControl.Robot.AbstractRobot.Config.DeviceConfig import DeviceConfig
 
@@ -15,8 +17,11 @@ class MotionController(HardwareDevice):
 
     def run(self):
         print("device - run")
-        while True:
-            sleep(1)
+        while True:    
+            input("\nHit enter to send ping")
+            data_packet = RemoteDataPacket(11, 1, 3)
+            data_packet.set_remote_data(RemoteData(300, 'The coolest', 'The coolest data'))
+            self._connection._data_output.transmit(data_packet)
 
     def connect(self, connection: PicoConnection) -> None:
         self._connection = connection
